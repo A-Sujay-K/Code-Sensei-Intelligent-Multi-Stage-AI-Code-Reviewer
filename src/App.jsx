ff17e79bd31c7ff6c27a94624fb764b14418f5ef
Akarsh Sujay Kumar <akarshsujaykumar.23is@saividya.ac.in>
2025-07-20 09:45:00 +0530

    Configure Monaco Editor theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Editor from '@monaco-editor/react';
-export default function App() { return <><Navbar/><div className='main'><div className='left'><Editor height='100%' defaultLanguage='javascript'/></div><div className='right'>Analysis</div></div></>; }
+export default function App() { return <><Navbar/><div className='main'><div className='left'><Editor height='100%' theme='vs-dark' defaultLanguage='javascript'/></div><div className='right'>Analysis</div></div></>; }
