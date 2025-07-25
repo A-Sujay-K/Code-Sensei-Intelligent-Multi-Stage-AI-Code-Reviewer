8e9cb03ac548218c5b480e7dc967d3206f8e4eeb
Akarsh Sujay Kumar <akarshsujaykumar.23is@saividya.ac.in>
2025-07-25 11:10:00 +0530

    Configure Monaco editor basic settings

--- src/App.jsx
+++ src/App.jsx
@@ -2,3 +2,4 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Editor from '@monaco-editor/react';
 export default function App() { return <><Navbar/><div className='main'><div className='left'><Editor height='100%' theme='vs-dark' defaultLanguage='javascript'/></div><div className='right'>Analysis</div></div></>; }
+// Added editor config options
